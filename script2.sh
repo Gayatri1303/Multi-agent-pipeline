bd41f83c3f4e923b11bfe7f5a44dcfe64b744bd6
Gayatri1303 <gayatrikurulkar@gmail.com>
2025-10-30 09:07:55 +0000

    added error handling for existing cont

--- script2.sh
+++ script2.sh
@@ -2,6 +2,8 @@ sudo apt update
 sudo apt install docker.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo usermod -aG docker jenkins
 sudo chmod 666 /var/run/docker.sock
 docker pull nginx:latest
-docker run -d -p 80:80 nginx:latest
+docker stop demo || true
+docker run -d -p 80:80 --name demo nginx:latest
