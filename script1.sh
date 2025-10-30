6a6c80ae97071e79c827e7fd8e3b5b7bc33f5724
Gayatri1303 <gayatrikurulkar@gmail.com>
2025-10-30 10:19:57 +0000

    script1 updated

--- script1.sh
+++ script1.sh
@@ -4,7 +4,7 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo chmod 666 /var/run/docker.sock
-docker pull gayatri491/frontend_app3:latest 
 docker stop frontendapp || true
 docker rm frontendapp || true
+docker pull gayatri491/frontend_app3:latest 
 docker run -d -p 80:80 --name frontendapp gayatri491/frontend_app3:latest
