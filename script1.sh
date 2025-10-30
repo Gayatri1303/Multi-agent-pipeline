0589eb7222f5bb1490611ee8fd37eb500a5eff16
Gayatri1303 <gayatrikurulkar@gmail.com>
2025-10-30 09:19:56 +0000

    added error handling for existing cont

--- script1.sh
+++ script1.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install docker.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo usermod -aG docker jenkins
+sudo usermod -aG docker ubuntu
 sudo chmod 666 /var/run/docker.sock
 docker pull gayatri491/frontend_app3:latest 
 docker stop frontendapp || true
