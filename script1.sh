49588d716dd98d79bf84ede0596d4165170a6899
Gayatri1303 <gayatrikurulkar@gmail.com>
2025-10-30 09:00:59 +0000

    modified scripts

--- script1.sh
+++ script1.sh
@@ -2,7 +2,6 @@ sudo apt update
 sudo apt install docker.io -y
 sudo systemctl enable docker
 sudo systemctl start docker
-sudo usermod -aG docker jenkins
 sudo chmod 666 /var/run/docker.sock
 docker pull gayatri491/frontend_app3:latest 
 docker run -d -p 80:80 gayatri491/frontend_app3:latest
